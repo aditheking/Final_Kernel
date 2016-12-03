ca6bb817e3391281e4946ffdf42ad3ddbf967630
AayushRd7 <aayush835@gmail.com>
2016-12-03 04:00:46 +0000

    Linux 3.4.12
    Signed-off-by: AayushRd7 <aayush835@gmail.com>
    
    Conflicts:
    	drivers/net/usb/asix.c
    	drivers/usb/host/xhci-hub.c
    	kernel/events/core.c
    	kernel/kthread.c
    	mm/vmscan.c

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -1853,8 +1853,8 @@ out:
 		unsigned long scan;
 
 		scan = zone_nr_lru_pages(mz, lru);
-		if (sc->priority || noswap || !vmscan_swappiness(sc)) {
-			scan >>= sc->priority;
+		if (priority || noswap || !vmscan_swappiness(mz, sc)) {
+			scan >>= priority;
 			if (!scan && force_scan)
 				scan = SWAP_CLUSTER_MAX;
 			scan = div64_u64(scan * fraction[file], denominator);

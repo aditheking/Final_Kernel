87a559852c7951264fed5adc655084843876be03
SoUnd001 <raduraducu200@gmail.com>
2017-01-23 18:35:38 +0200

    Move to arm-eabi 4.9

--- build_msm8x26.sh
+++ build_msm8x26.sh
@@ -7,7 +7,7 @@ BUILD_TOP_DIR=..
 BUILD_KERNEL_DIR=$(pwd)
 
 echo "Define YOUR OWN cross compile path"
-BUILD_CROSS_COMPILE=/media/lukag/UCompile/cm12/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
+BUILD_CROSS_COMPILE=~/working/kernel/arm-eabi-4.9/bin/arm-eabi-
 BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 KERNEL_DEFCONFIG=msm8226-sec_defconfig

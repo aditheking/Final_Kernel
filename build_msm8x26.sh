e3b9cd632a5b9a11b85dc911db4d81837dc1affc
aditheking <aupreti72@gmail.com>
2017-01-28 14:30:58 +0000

    Update toolchain

--- build_msm8x26.sh
+++ build_msm8x26.sh
@@ -6,7 +6,7 @@
 BUILD_TOP_DIR=..
 BUILD_KERNEL_DIR=$(pwd)
 
-BUILD_CROSS_COMPILE=~/working/kernel/arm-eabi-4.9/bin/arm-eabi-
+BUILD_CROSS_COMPILE=$(pwd)/arm-eabi-4.9/bin/arm-eabi-
 BUILD_JOB_NUMBER=`grep processor /proc/cpuinfo|wc -l`
 
 KERNEL_DEFCONFIG=msm8226-sec_defconfig
